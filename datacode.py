8b81f957ec2f9604952c9513616853c30c17040a
Manohar <manoharnimiditalli001@gmail.com>
2025-03-11 14:47:05 +0530

    3rd version of data saved

--- datacode.py
+++ datacode.py
@@ -20,6 +20,9 @@ df.loc[len(df.index)] = new_row_loc2
 new_row_loc3 = {'Name': 'natalie', 'Age': 27, 'City': 'australia'}
 df.loc[len(df.index)] = new_row_loc3
 
+new_row_loc4 = {'Name': 'meg', 'Age': 27, 'City': 'new zealand'}
+df.loc[len(df.index)] = new_row_loc4
+
 # Ensure the "data" directory exists at the root level
 data_dir = 'data'
 os.makedirs(data_dir, exist_ok=True)
